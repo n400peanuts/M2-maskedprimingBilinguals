a85c763f27e54edd403f57ea18bdc0936c5dd3ea
Eva Viviani <eva_viviani@msn.com>
2017-01-13 17:34:57 +0100

    added another file

---
+++ bilingualDiagnostics.R
@@ -0,0 +1,52 @@
+# ---- Diagnostics.Rfunction of Davide Crepaldi (see: http://www.davidecrepaldi.net/wordpress/software-utilities-2/)
+# ---- 13/01/2017
+
+#---------------------------------------------------------------------------------------------------#
+#-------------------------------------ITA f.diagnostics---------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+subset(masterFile, Language=="ita")-> masterfileIta
+
+
+sbj.id<- masterfileIta$Subject
+acc<- masterfileIta$Accuracy
+lexicality<- masterfileIta$Lexicality
+target<-masterfileIta$Target
+rt<-masterfileIta$rt
+
+setwd("")
+
+source("diagnostics.r")
+diagnostics.f(rt, acc, sbj.id, target, lexicality, ita)
+
+#Subj. 16 has reported seeing clearly the prime, for this reason will be taken out of the analysis, in addition to the words that scored less than 30% of accuracy. 
+# We filter also Rts from 250 to 1600ms
+subset(target.diagnostics, acc<0.7)-> parolebrutte #.giusto per sapere quali sono
+subset(masterfileIta, masterfileIta$rt>250 & masterfileIta$rt<1600 & masterfileIta$Subject!=16 & masterfileIta$Target!= "congruo" & masterfileIta$Target!= "guado" & masterfileIta$Target!= "guano" & masterfileIta$Target!= "uggia" & masterfileIta$Target!= "vello" & masterfileIta$Target!= "avo" & masterfileIta$Lexicality=="WORD") -> dataAccITA
+#Then, we select only right answers
+subset(dataAccITA, dataAccITA$Accuracy==1)-> datartITA
+summary(datartITA)
+#First look at the means
+round(xtabs(datartITA$rt ~ datartITA$Morphtype + datartITA$Primetype) / xtabs(~datartITA$Morphtype + datartITA$Primetype), digits = 0)
+
+#---------------------------------------------------------------------------------------------------#
+#-------------------------------------ENG f.diagnostics---------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+subset(masterFile, Language=="eng")-> masterfileEng
+
+
+sbj.id<- masterfileEng$Subject
+acc<- masterfileEng$Accuracy
+lexicality<- masterfileEng$Lexicality
+target<-masterfileEng$Target
+rt<-masterfileEng$rt
+
+diagnostics.f(rt, acc, sbj.id, target, lexicality, eng)
+
+#Subjs 22 and 26 will be taken out because their performance is very far from the others and the accuracy less than 40%, see the output graph from the diagnostics function.
+# We filter also Rts from 250 to 1900ms
+subset(masterfileEng, masterfileEng$rt>250 & masterfileEng$rt<1900 & masterfileEng$Subject!=22 & masterfileEng$Subject!=26 & masterfileEng$Lexicality=="WORD") -> dataAccENG
+#Then, we select only right answers
+subset(dataAccENG, dataAccENG$Accuracy==1)-> datartENG
+#First look at the means
+round(xtabs(datartENG$rt ~ datartENG$Morphtype + datartENG$Primetype) / xtabs(~datartENG$Morphtype + datartENG$Primetype), digits = 0)
+
